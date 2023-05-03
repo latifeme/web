69beec05c0236af8c8932496edc5ba0b691e4545
薄荷牛奶萌 <101342651+MoeCinnamo@users.noreply.github.com>
2023-05-03 13:53:08 +0800

    Create spa.js

---
+++ js/spa.js
@@ -0,0 +1,30 @@
+// 定义 Router  
+class Router {  
+    constructor () {  
+        this.routes = {};  
+        this.listerPopState()  
+    }  
+    init(path) {  
+        history.replaceState({path: path}, null, path);  
+        this.routes[path] && this.routes[path]();  
+    }  
+    route(path, callback){  
+        this.routes[path] = callback;  
+    }  
+    push(path) {  
+        history.pushState({path: path}, null, path);  
+        this.routes[path] && this.routes[path]();  
+    }  
+    listerPopState () {  
+        window.addEventListener('popstate' , e => {  
+            const path = e.state && e.state.path;  
+            this.routers[path] && this.routers[path]()  
+        })  
+    }  
+}  
+// 使用 Router  
+window.miniRouter = new Router();  
+miniRouter.route('/', ()=> console.log('page1'))  
+miniRouter.route('/page2', ()=> console.log('page2'))  
+// 跳转  
+miniRouter.push('/page2')  // page2
